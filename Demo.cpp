95048f57f6018d8f67fd4de8ceb2fe9a14be735d
BenjaminSaldman <93070344+BenjaminSaldman@users.noreply.github.com>
2024-06-11 20:57:06 +0300

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -11,60 +11,72 @@ using namespace std;
 int main()
 {
 
-    Node<string> root_node = Node("root");
-    Tree tree;
+    Node<string> root_node = Node(1.1);
+    Tree<double> tree; // Binary tree that contains doubles.
     tree.add_root(root_node);
-    Node<int> n1 = Node(1);
-    Node<string> n2 = Node("n2");
-    Node<double> n3 = Node(12.35);
-    Node<double> n4 = Node(1 / 4);
-    Node<string> n5 = Node("n5");
-    Node<string> n6 = Node("n6");
-    Node<string> n7 = Node("n7");
+    Node<double> n1 = Node(1.2);
+    Node<double> n2 = Node(1.3);
+    Node<double> n3 = Node(1.4);
+    Node<double> n4 = Node(1.5);
+    Node<double> n5 = Node(1.6);
 
     tree.add_sub_node(root_node, n1);
     tree.add_sub_node(root_node, n2);
-    tree.add_sub_node(root_node, n3);
+    tree.add_sub_node(n1, n3);
     tree.add_sub_node(n1, n4);
-    tree.add_sub_node(n1, n5);
-    tree.add_sub_node(n2, n6);
-    tree.add_sub_node(n2, n7);
+    tree.add_sub_node(n2, n5);
+   
     // The tree should look like:
     /**
-     * root
-     * |__1
-     * |  |__0.25
-     * |  |__n5
-     * |__n2
-     * |  |__n6
-     * |  |__n7
-     * |__12.35
+     *       root = 1.1
+     *     /       \
+     *    1.2      1.3
+     *   /  \      /
+     *  1.4  1.5  1.6
      */
 
     for (auto node = tree.begin_pre_order(); node != tree.end_pre_order(); ++node)
     {
         cout << node->get_value() << endl;
-    } // prints: root, 1, 0.25, n5, n2, n6, n7, 12.35
+    } // prints: 1.1, 1.2, 1.4, 1.5, 1.3, 1.6
 
     for (auto node = tree.begin_post_order(); node != tree.end_post_order(); ++node)
     {
         cout << node->get_value() << endl;
-    } // prints: 0.25, n5, 1, n6, n7, n2, 12.35, root
+    } // prints: 1.4, 1.5, 1.2, 1.6, 1.3, 1.1
 
     for (auto node = tree.begin_in_order(); node != tree.end_in_order(); ++node)
     {
         cout << node->get_value() << endl;
-    } // prints: 0.25, 1, n5, root, n6, n2, n7, 12.35
+    } // prints: 1.4, 1.2, 1.5, 1.1, 1.6, 1.3
 
     for (auto node = tree.begin_bfs_scan(); node != tree.end_bfs_scan(); ++node)
     {
         cout << node->get_value() << endl;
-    } // prints: root, 1, n2, 12.35, 0.25, n5, n6, n7
+    } // prints: 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
 
     for (auto node : tree)
     {
         cout << node.get_value() << endl;
-    } // same as BFS: root, 1, n2, 12.35, 0.25, n5, n6, n7
+    } // same as BFS: 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
 
     cout << tree; // Should print the graph using GUI.
+
+    Tree<int,3> three_ary_tree; // 3-ary tree.
+    three_ary_tree.add_root(root_node);
+    three_ary_tree.add_sub_node(root_node, n1);
+    three_ary_tree.add_sub_node(root_node, n2);
+    three_ary_tree.add_sub_node(root_node, n3);
+    three_ary_tree.add_sub_node(n1, n4);
+    three_ary_tree.add_sub_node(n2, n5);
+
+     // The tree should look like:
+    /**
+     *       root = 1.1
+     *     /      |     \
+     *    1.2    1.3    1.4
+     *   /        |
+     *  1.5      1.6
+     */
+    
 }
